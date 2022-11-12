4dff91c51207fb7698847705947668db308aba04
Dexit-Network <96398816+Dexit-Network@users.noreply.github.com>
2022-11-13 09:19:21 +1100

    Delete static directory

--- static/js/6.da85a05b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpname=this.webpackJsonpname||[]).push([[6],{978:function(n,p){}}]);
-//# sourceMappingURL=6.da85a05b.chunk.js.map
--- static/js/7.bf0b99d0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpname=this.webpackJsonpname||[]).push([[7],{1092:function(n,p){}}]);
-//# sourceMappingURL=7.bf0b99d0.chunk.js.map
